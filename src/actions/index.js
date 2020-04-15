aecfea5b8d7f497e442ab59ad510d5996dab94ec
tarasenkoDm <dm.a.tarasenko@gmail.com>
2020-04-14 22:34:38 -0400

    moved jsonPlaceholder to apis

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import jsonPlaceholder from "../jsonPlaceholder";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get('/posts');
