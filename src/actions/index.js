099a6fcbde9741701ae9e3bfe076cf76c374c1d0
tarasenkoDm <dm.a.tarasenko@gmail.com>
2020-04-16 17:42:01 -0400

    added fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,9 @@ export const fetchPosts = () => async (dispatch) => {
 		payload: response.data
 	})
 }
+
+export const fetchUser = (id) => async (dispatch) => {
+	const response = await jsonPlaceholder.get(`/user/${id}`);
+
+	dispatch({type: 'FETCH_USER', payload: response.data})
+}
