004eb1c42c3ee45204d02bd4881ed141ac87d4fb
tarasenkoDm <dm.a.tarasenko@gmail.com>
2020-04-16 22:26:24 -0400

    1)added usersReducer, 2)added usersReducer to combineReducers;  3)Added MapStateToProps for UserHeader.component

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,12 +8,20 @@ class UserHeader extends React.Component {
 	}
 
 	render() {
+		const user = this.props.users.find(user => user.id === this.props.userId);
+		if (!user) {
+			return null;
+		}
 		return (
-			<div>
-				User Header
+			<div className="header">
+				{user.name}
 			</div>
 		);
 	}
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state) => {
+	return { users: state.users };
+}
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
