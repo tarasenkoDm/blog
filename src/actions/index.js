398d5df06f9ca3a8ab29da2899546458b8ea67b0
tarasenkoDm <dm.a.tarasenko@gmail.com>
2020-04-19 20:31:04 -0400

    added alternate approach to find and dispatch userId using lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,17 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 	await dispatch(fetchPosts());
 
+	//first approach
 	const userIds = _.uniq(_.map(getState().posts, 'userId'));
 	userIds.forEach(id => dispatch(fetchUser(id)));
+
+	// alternate approach to find and dispatch userId using lodash
+
+	// _.chain(getState().posts)
+	// 	.map('userId')
+	// 	.uniq()
+	// 	.forEach(id => dispatch(fetchUser(id)))
+	// 	.value();
 }
 
 export const fetchPosts = () => async (dispatch) => {
