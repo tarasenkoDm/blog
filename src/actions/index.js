96598abf056de0ae7ea5bb6a5e17d09eab3642ce
tarasenkoDm <dm.a.tarasenko@gmail.com>
2020-04-19 20:19:57 -0400

    Alternate overfetching solution 1) added new action creator: fetchPostsAndUsers; 2) Used it in PostList 3) deleted componentDidMount from UserHeader

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+
+	const userIds = _.uniq(_.map(getState().posts, 'userId'));
+	userIds.forEach(id => dispatch(fetchUser(id)));
+}
+
 export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get('/posts');
 
@@ -10,12 +17,9 @@ export const fetchPosts = () => async (dispatch) => {
 	})
 }
 
-export const fetchUser = (id) => (dispatch) => {
-	_fetchUser(id, dispatch);
-}
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+//second approach
+export const fetchUser = (id) => async  (dispatch) => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 
 	dispatch({type: 'FETCH_USER', payload: response.data})
-})
+}
